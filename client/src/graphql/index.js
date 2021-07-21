63a867160a65a6dedc500f2dd679f882cf6b0a49
Galuh Alifani <galuh.adika@gmail.com>
2021-07-21 20:56:52 +0700

    index edit

--- client/src/graphql/index.js
+++ client/src/graphql/index.js
@@ -1,6 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+// export const client = new ApolloClient({
+//     uri: 'http://localhost:4000',
+//     cache: new InMemoryCache()
+// });
+
 export const client = new ApolloClient({
-    uri: 'http://localhost:4000',
+    uri: 'http://34.204.82.162:4000',
     cache: new InMemoryCache()
 });
